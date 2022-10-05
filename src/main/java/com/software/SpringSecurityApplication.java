96c0c8540dafbb553e41b9b08bab6564f58047d5
wanghao <281463547@qq.com>
2022-10-05 21:55:27 +0800

    swagger、fastjson调整

--- src/main/java/com/software/SpringSecurityApplication.java
+++ src/main/java/com/software/SpringSecurityApplication.java
@@ -3,13 +3,15 @@ package com.software;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Wang Hao
  * @date 2022/10/5 12:42
  */
-@SpringBootApplication
+@EnableSwagger2
 @MapperScan({"com.software.mapper"})
+@SpringBootApplication
 public class SpringSecurityApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityApplication.class, args);
