f45884be04329b2a648b28d28ce756828c863a3b
wanghao <281463547@qq.com>
2022-10-05 22:51:20 +0800

    Redis工具类修改

--- src/main/java/com/software/utils/RedisUtil.java
+++ src/main/java/com/software/utils/RedisUtil.java
@@ -1,10 +1,11 @@
 package com.software.utils;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.common.collect.HashMultimap;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.*;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -14,61 +15,23 @@ import java.util.concurrent.TimeUnit;
  */
 public class RedisUtil {
 
-    @Autowired
-    private RedisTemplate<String, Object> redisTemplate;
+    private StringRedisTemplate redisTemplate;
 
-    private static double size = Math.pow(2, 32);
-
-
-    /**
-     * 写入缓存
-     *
-     * @param key
-     * @param offset 位 8Bit=1Byte
-     * @return
-     */
-    public boolean setBit(String key, long offset, boolean isShow) {
-        boolean result = false;
-        try {
-            ValueOperations<String, Object> operations = redisTemplate.opsForValue();
-            operations.setBit(key, offset, isShow);
-            result = true;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
+    public RedisUtil(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
     }
 
     /**
      * 写入缓存
      *
-     * @param key
-     * @param offset
-     * @return
+     * @param key   redis键
+     * @param value redis值
+     * @return 是否成功
      */
-    public boolean getBit(String key, long offset) {
+    public boolean set(final String key, String value) {
         boolean result = false;
         try {
-            ValueOperations<String, Object> operations = redisTemplate.opsForValue();
-            result = operations.getBit(key, offset);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-
-    /**
-     * 写入缓存
-     *
-     * @param key
-     * @param value
-     * @return
-     */
-    public boolean set(final String key, Object value) {
-        boolean result = false;
-        try {
-            ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+            ValueOperations<String, String> operations = redisTemplate.opsForValue();
             operations.set(key, value);
             result = true;
         } catch (Exception e) {
@@ -77,18 +40,17 @@ public class RedisUtil {
         return result;
     }
 
-
     /**
-     * 写入缓存设置失效时间
+     * 写入缓存设置时效时间
      *
-     * @param key
-     * @param value
-     * @return
+     * @param key   redis键
+     * @param value redis值
+     * @return 是否成功
      */
-    public boolean set(final String key, Object value, Long expireTime) {
+    public boolean set(final String key, String value, Long expireTime) {
         boolean result = false;
         try {
-            ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+            ValueOperations<String, String> operations = redisTemplate.opsForValue();
             operations.set(key, value);
             redisTemplate.expire(key, expireTime, TimeUnit.SECONDS);
             result = true;
@@ -99,21 +61,32 @@ public class RedisUtil {
     }
 
     /**
-     * 批量删除对应的value
+     * 批量删除对应的键值对
      *
-     * @param keys
+     * @param keys Redis键名数组
      */
-    public void remove(final String... keys) {
+    public void removeByKeys(final String... keys) {
         for (String key : keys) {
             remove(key);
         }
     }
 
+    /**
+     * 批量删除Redis key
+     *
+     * @param pattern 键名包含字符串（如：myKey*）
+     */
+    public void removePattern(final String pattern) {
+        Set<String> keys = redisTemplate.keys(pattern);
+        if (keys != null && keys.size() > 0) {
+            redisTemplate.delete(keys);
+        }
+    }
 
     /**
-     * 删除对应的value
+     * 删除key,也删除对应的value
      *
-     * @param key
+     * @param key Redis键名
      */
     public void remove(final String key) {
         if (exists(key)) {
@@ -124,22 +97,22 @@ public class RedisUtil {
     /**
      * 判断缓存中是否有对应的value
      *
-     * @param key
-     * @return
+     * @param key Redis键名
+     * @return 是否存在
      */
-    public boolean exists(final String key) {
+    public Boolean exists(final String key) {
         return redisTemplate.hasKey(key);
     }
 
     /**
      * 读取缓存
      *
-     * @param key
-     * @return
+     * @param key Redis键名
+     * @return 是否存在
      */
-    public Object get(final String key) {
-        Object result = null;
-        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+    public String get(final String key) {
+        String result = null;
+        ValueOperations<String, String> operations = redisTemplate.opsForValue();
         result = operations.get(key);
         return result;
     }
@@ -147,182 +120,293 @@ public class RedisUtil {
     /**
      * 哈希 添加
      *
-     * @param key
-     * @param hashKey
-     * @param value
+     * @param key     Redis键
+     * @param hashKey 哈希键
+     * @param value   哈希值
      */
-    public void hmSet(String key, Object hashKey, Object value) {
-        HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
+    public void hmSet(String key, String hashKey, String value) {
+        HashOperations<String, String, String> hash = redisTemplate.opsForHash();
         hash.put(key, hashKey, value);
     }
 
     /**
      * 哈希获取数据
      *
-     * @param key
-     * @param hashKey
-     * @return
+     * @param key     Redis键
+     * @param hashKey 哈希键
+     * @return 哈希值
      */
-    public Object hmGet(String key, Object hashKey) {
-        HashOperations<String, Object, Object> hash = redisTemplate.opsForHash();
+    public String hmGet(String key, String hashKey) {
+        HashOperations<String, String, String> hash = redisTemplate.opsForHash();
         return hash.get(key, hashKey);
     }
 
     /**
-     * 列表添加
+     * 判断hash是否存在键
      *
-     * @param k
-     * @param v
+     * @param key     Redis键
+     * @param hashKey 哈希键
+     * @return 是否存在
      */
-    public void lPush(String k, Object v) {
-        ListOperations<String, Object> list = redisTemplate.opsForList();
-        list.rightPush(k, v);
+    public boolean hmHasKey(String key, String hashKey) {
+        HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+        return hash.hasKey(key, hashKey);
     }
 
     /**
-     * 列表获取
+     * 删除hash中一条或多条数据
      *
-     * @param k
-     * @param l
-     * @param l1
-     * @return
+     * @param key      Redis键
+     * @param hashKeys 哈希键名数组
+     * @return 删除数量
      */
-    public List<Object> lRange(String k, long l, long l1) {
-        ListOperations<String, Object> list = redisTemplate.opsForList();
-        return list.range(k, l, l1);
+    public long hmRemove(String key, String... hashKeys) {
+        HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+        return hash.delete(key, hashKeys);
+    }
+
+    /**
+     * 获取所有哈希键值对
+     *
+     * @param key Redis键名
+     * @return 哈希Map
+     */
+    public Map<String, String> hashMapGet(String key) {
+        HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+        return hash.entries(key);
+    }
+
+    /**
+     * 保存Map到哈希
+     *
+     * @param key Redis键名
+     * @param map 哈希Map
+     */
+    public void hashMapSet(String key, Map<String, String> map) {
+        HashOperations<String, String, String> hash = redisTemplate.opsForHash();
+        hash.putAll(key, map);
+    }
+
+    /**
+     * 列表-追加值
+     *
+     * @param key   Redis键名
+     * @param value 列表值
+     */
+    public void lPush(String key, String value) {
+        ListOperations<String, String> list = redisTemplate.opsForList();
+        list.leftPush(key, value);
+    }
+
+    /**
+     * 列表-删除值
+     *
+     * @param key   Redis键名
+     * @param value 列表值
+     */
+    public void lRemove(String key, String value) {
+        ListOperations<String, String> list = redisTemplate.opsForList();
+        list.remove(key, 0, value);
+    }
+
+
+    /**
+     * 列表-获取指定范围数据
+     *
+     * @param key   Redis键名
+     * @param start 开始行号（start:0，end:-1查询所有值）
+     * @param end   结束行号
+     * @return 列表
+     */
+    public List<String> lRange(String key, long start, long end) {
+        ListOperations<String, String> list = redisTemplate.opsForList();
+        return list.range(key, start, end);
     }
 
     /**
      * 集合添加
      *
-     * @param key
-     * @param value
+     * @param key   Redis键名
+     * @param value 值
      */
-    public void add(String key, Object value) {
-        SetOperations<String, Object> set = redisTemplate.opsForSet();
+    public void add(String key, String value) {
+        SetOperations<String, String> set = redisTemplate.opsForSet();
         set.add(key, value);
     }
 
     /**
      * 集合获取
      *
-     * @param key
-     * @return
+     * @param key Redis键名
+     * @return 集合
      */
-    public Set<Object> setMembers(String key) {
-        SetOperations<String, Object> set = redisTemplate.opsForSet();
+    public Set<String> setMembers(String key) {
+        SetOperations<String, String> set = redisTemplate.opsForSet();
         return set.members(key);
     }
 
     /**
      * 有序集合添加
      *
-     * @param key
-     * @param value
-     * @param scoure
-     */
-    public void zAdd(String key, Object value, double scoure) {
-        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        zset.add(key, value, scoure);
-    }
-
-    /**
-     * 有序集合获取
-     *
-     * @param key
-     * @param scoure
-     * @param scoure1
-     * @return
-     */
-    public Set<Object> rangeByScore(String key, double scoure, double scoure1) {
-        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        redisTemplate.opsForValue();
-        return zset.rangeByScore(key, scoure, scoure1);
-    }
-
-
-    //第一次加载的时候将数据加载到redis中
-    public void saveDataToRedis(String name) {
-        double index = Math.abs(name.hashCode() % size);
-        long indexLong = new Double(index).longValue();
-        boolean availableUsers = setBit("availableUsers", indexLong, true);
-    }
-
-    //第一次加载的时候将数据加载到redis中
-    public boolean getDataToRedis(String name) {
-
-        double index = Math.abs(name.hashCode() % size);
-        long indexLong = new Double(index).longValue();
-        return getBit("availableUsers", indexLong);
-    }
-
-    /**
-     * 有序集合获取排名
-     *
-     * @param key   集合名称
+     * @param key   Redis键名
      * @param value 值
+     * @param score 排序号
      */
-    public Long zRank(String key, Object value) {
-        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        return zset.rank(key, value);
-    }
-
-
-    /**
-     * 有序集合获取排名
-     *
-     * @param key
-     */
-    public Set<ZSetOperations.TypedTuple<Object>> zRankWithScore(String key, long start, long end) {
-        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        Set<ZSetOperations.TypedTuple<Object>> ret = zset.rangeWithScores(key, start, end);
-        return ret;
+    public void zAdd(String key, String value, double score) {
+        ZSetOperations<String, String> zSet = redisTemplate.opsForZSet();
+        zSet.add(key, value, score);
     }
 
     /**
-     * 有序集合添加
+     * 有序集合-获取指定范围
      *
-     * @param key
-     * @param value
+     * @param key        Redis键
+     * @param startScore 开始序号
+     * @param endScore   结束序号
+     * @return 集合
      */
-    public Double zSetScore(String key, Object value) {
-        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        return zset.score(key, value);
-    }
-
-
-    /**
-     * 有序集合添加分数
-     *
-     * @param key
-     * @param value
-     * @param scoure
-     */
-    public void incrementScore(String key, Object value, double scoure) {
-        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        zset.incrementScore(key, value, scoure);
-    }
-
-
-    /**
-     * 有序集合获取排名
-     *
-     * @param key
-     */
-    public Set<ZSetOperations.TypedTuple<Object>> reverseZRankWithScore(String key, long start, long end) {
-        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        Set<ZSetOperations.TypedTuple<Object>> ret = zset.reverseRangeByScoreWithScores(key, start, end);
-        return ret;
+    public Set<String> rangeByScore(String key, double startScore, double endScore) {
+        ZSetOperations<String, String> zset = redisTemplate.opsForZSet();
+        return zset.rangeByScore(key, startScore, endScore);
     }
 
     /**
-     * 有序集合获取排名
+     * 模糊查询Redis键名
      *
-     * @param key
+     * @param pattern 键名包含字符串（如：myKey*）
+     * @return 集合
      */
-    public Set<ZSetOperations.TypedTuple<Object>> reverseZRankWithRank(String key, long start, long end) {
-        ZSetOperations<String, Object> zset = redisTemplate.opsForZSet();
-        Set<ZSetOperations.TypedTuple<Object>> ret = zset.reverseRangeWithScores(key, start, end);
-        return ret;
+    public Set<String> keys(String pattern) {
+        return redisTemplate.keys(pattern);
+    }
+
+    /**
+     * 获取多个hashMap
+     *
+     * @param keySet
+     * @return List<Map < String, String>> hashMap列表
+     */
+    public List hashMapList(Collection<String> keySet) {
+        return redisTemplate.executePipelined(new SessionCallback<String>() {
+            @Override
+            public <K, V> String execute(RedisOperations<K, V> operations) throws DataAccessException {
+                HashOperations hashOperations = operations.opsForHash();
+                for (String key : keySet) {
+                    hashOperations.entries(key);
+                }
+                return null;
+            }
+        });
+    }
+
+    /**
+     * 保存多个哈希表（HashMap）(Redis键名可重复)
+     *
+     * @param batchMap Map<Redis键名,Map<键,值>>
+     */
+    public void batchHashMapSet(HashMultimap<String, Map<String, String>> batchMap) {
+        // 设置5秒超时时间
+        redisTemplate.expire("max", 25, TimeUnit.SECONDS);
+        redisTemplate.executePipelined(new RedisCallback<List<Map<String, String>>>() {
+
+            @Override
+            public List<Map<String, String>> doInRedis(RedisConnection connection) throws DataAccessException {
+                Iterator<Map.Entry<String, Map<String, String>>> iterator = batchMap.entries().iterator();
+                while (iterator.hasNext()) {
+                    Map.Entry<String, Map<String, String>> hash = iterator.next();
+                    // 哈希名,即表名
+                    byte[] hashName = redisTemplate.getStringSerializer().serialize(hash.getKey());
+                    Map<String, String> hashValues = hash.getValue();
+                    Iterator<Map.Entry<String, String>> it = hashValues.entrySet().iterator();
+                    // 将元素序列化后缓存，即表的多条哈希记录
+                    Map<byte[], byte[]> hashes = new HashMap<byte[], byte[]>();
+                    while (it.hasNext()) {
+                        // hash中一条key-value记录
+                        Map.Entry<String, String> entry = it.next();
+                        byte[] key = redisTemplate.getStringSerializer().serialize(entry.getKey());
+                        byte[] value = redisTemplate.getStringSerializer().serialize(entry.getValue());
+                        hashes.put(key, value);
+                    }
+                    // 批量保存
+                    connection.hMSet(hashName, hashes);
+                }
+                return null;
+            }
+        });
+    }
+
+    /**
+     * 保存多个哈希表（HashMap）(Redis键名不可以重复)
+     *
+     * @param dataMap Map<Redis键名,Map<哈希键,哈希值>>
+     */
+    public void batchHashMapSet(Map<String, Map<String, String>> dataMap) {
+        // 设置5秒超时时间
+        redisTemplate.expire("max", 25, TimeUnit.SECONDS);
+        redisTemplate.executePipelined(new RedisCallback<List<Map<String, String>>>() {
+
+            @Override
+            public List<Map<String, String>> doInRedis(RedisConnection connection) throws DataAccessException {
+                Iterator<Map.Entry<String, Map<String, String>>> iterator = dataMap.entrySet().iterator();
+                while (iterator.hasNext()) {
+                    Map.Entry<String, Map<String, String>> hash = iterator.next();
+                    // 哈希名,即表名
+                    byte[] hashName = redisTemplate.getStringSerializer().serialize(hash.getKey());
+                    Map<String, String> hashValues = hash.getValue();
+                    Iterator<Map.Entry<String, String>> it = hashValues.entrySet().iterator();
+                    // 将元素序列化后缓存，即表的多条哈希记录
+                    Map<byte[], byte[]> hashes = new HashMap<byte[], byte[]>();
+                    while (it.hasNext()) {
+                        // hash中一条key-value记录
+                        Map.Entry<String, String> entry = it.next();
+                        byte[] key = redisTemplate.getStringSerializer().serialize(entry.getKey());
+                        byte[] value = redisTemplate.getStringSerializer().serialize(entry.getValue());
+                        hashes.put(key, value);
+                    }
+                    // 批量保存
+                    connection.hMSet(hashName, hashes);
+                }
+                return null;
+            }
+        });
+    }
+
+    /**
+     * 保存多个哈希表（HashMap）列表（哈希map的Redis键名不能重复）
+     *
+     * @param list Map<Redis键名,Map<哈希键,哈希值>>
+     * @see RedisUtil*.batchHashMapSet()*
+     */
+    public void batchHashMapListSet(List<Map<String, Map<String, String>>> list) {
+        // 设置5秒超时时间
+        redisTemplate.expire("max", 25, TimeUnit.SECONDS);
+        redisTemplate.executePipelined(new RedisCallback<List<Map<String, String>>>() {
+
+            @Override
+            public List<Map<String, String>> doInRedis(RedisConnection connection) throws DataAccessException {
+                for (Map<String, Map<String, String>> dataMap : list) {
+                    Iterator<Map.Entry<String, Map<String, String>>> iterator = dataMap.entrySet().iterator();
+                    while (iterator.hasNext()) {
+                        Map.Entry<String, Map<String, String>> hash = iterator.next();
+                        // 哈希名,即表名
+                        byte[] hashName = redisTemplate.getStringSerializer().serialize(hash.getKey());
+                        Map<String, String> hashValues = hash.getValue();
+                        Iterator<Map.Entry<String, String>> it = hashValues.entrySet().iterator();
+                        // 将元素序列化后缓存，即表的多条哈希记录
+                        Map<byte[], byte[]> hashes = new HashMap<byte[], byte[]>();
+                        while (it.hasNext()) {
+                            // hash中一条key-value记录
+                            Map.Entry<String, String> entry = it.next();
+                            byte[] key = redisTemplate.getStringSerializer().serialize(entry.getKey());
+                            byte[] value = redisTemplate.getStringSerializer().serialize(entry.getValue());
+                            hashes.put(key, value);
+                        }
+                        // 批量保存
+                        connection.hMSet(hashName, hashes);
+                    }
+                }
+                return null;
+            }
+        });
     }
 }
